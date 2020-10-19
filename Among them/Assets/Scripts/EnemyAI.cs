30b9f5a5c8e9cb3f60636a9cfb3cbd5aba1e26ff
Joethedinosore <Topherabarta@gmail.com>
2020-10-19 11:37:27 -0700

    I don't know what I fixed

---
+++ Among them/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent)), RequireComponent(typeof(NavMeshObstacle))]
+public class EnemyAI : MonoBehaviour
+{
+    [Header("Readouts")]
+    public int lastTask;
+
+    [Header("Interfaces")]
+    public Transform[] tasks;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        getNextTask();
+    }
+
+    public void OnReachTask(float delay, float taskValue)
+    {
+        StartCoroutine(waitForTask(delay, taskValue));
+    }
+
+    void getNextTask()
+    {
+        int nextTesk = getNextIndex();
+        lastTask = nextTesk;
+
+        agent.SetDestination(tasks[nextTesk].position);
+    }
+
+    int getNextIndex()
+    {
+        //get random task
+        int x = Random.Range(0, tasks.Length);
+
+        //try again if it is the last done task
+        if (x == lastTask)
+        {
+            return (getNextIndex());
+        }
+
+        //else, return x
+        return (x);
+    }
+
+    IEnumerator waitForTask(float delay, float taskValue)
+    {
+        //wait the delay
+        yield return new WaitForSeconds(delay);
+
+        //add the task value to the counter
+        //up task counter
+
+        //start pathing to the next task
+        getNextTask();
+    }
+}
---
+++ Among them/Assets/Scripts/Task.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    public float delay;
+
+    public float taskValue;
+
+    public int id;
+
+    //trigger when the task's aoe is entered
+    private void OnTriggerEnter(Collider other)
+    {
+        //check if it was an enemy
+        if(other.gameObject.tag == "Enemy")
+        {
+            //check if this task was their target
+            if (other.gameObject.GetComponent<EnemyAI>().lastTask == id)
+            {
+                //trigger target event
+                other.gameObject.GetComponent<EnemyAI>().OnReachTask(delay, taskValue);
+            }
+        }
+    }
+}
