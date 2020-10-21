3ffe85d31d36230390d64eeb21f0f60b794bde25
Joethedinosore <Topherabarta@gmail.com>
2020-10-21 10:22:39 -0700

    meet the crew

---
+++ Among them/Assets/Scripts/WanderAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderAI : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    public Transform target;
+
+    public Transform targetParent;
+
+    public GameObject targetFab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Instantiate(targetFab, targetParent).GetComponent<Transform>();
+        target.gameObject.GetComponent<WanderMarker>().parentObject = gameObject;
+        target.position = transform.position;
+        updateTarget();
+    }
+
+    public void updateTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Among them/Assets/Scripts/WanderMarker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderMarker : MonoBehaviour
+{
+    public Vector2 xRange, zRange;
+    public LayerMask layerMask;
+    public GameObject parentObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == parentObject)
+        {
+            transform.position = checkGround();
+            parentObject.GetComponent<WanderAI>().updateTarget();
+        }
+    }
+
+    private Vector3 checkGround()
+    {
+        float x = Random.Range(xRange.x, xRange.y);
+        float y = Random.Range(zRange.x, zRange.y);
+
+        Vector3 newPoz = new Vector3(x, 1, y);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(newPoz, Vector3.down, out hit, Mathf.Infinity, layerMask))
+        {
+            return (newPoz);
+        }
+        else
+        {
+            return (checkGround());
+        }
+    }
+}
